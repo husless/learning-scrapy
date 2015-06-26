1d75ebaafc71036e8bbeb717ff13f4ea6cc4216f
Huang Zhi <huszhi@gmail.com>
2015-06-26 21:32:25 +0800

    pagination

--- stack/pipelines.py
+++ stack/pipelines.py
@@ -27,7 +27,8 @@ class MongoDBPipeline(object):
                 valid = False
                 raise DropItem('Missing {0}'.format(data))
         if valid:
-            self.collection.insert(dict(item))
+            self.collection.update({'url': item['url']},
+                                   dict(item), upsert=True)
             logging.log(logging.INFO, 'Question added to MongoDB database!',
                     spider=spider)
         return item
